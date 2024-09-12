e8e6fb44d966a3da237b5fa9c25d9afbc73b2b46
Ryan <ryanarajo@yahoo.com.br>
2024-09-11 22:51:51 -0300

    func: add modelos Base, Alerta e HistoricoAlerta

---
+++ src/tupan/alertas/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from datetime import datetime
+
+
+class Base(models.Model):
+    criado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Alerta(Base):
+    nome = models.CharField(help_text="nome do alerta", max_length=127, unique=True)
+    condicao = models.CharField(help_text="condição para o alerta acontecer", max_length=4)
+    ativo = models.BooleanField(default=True)
+# falta a chave estrangeira da estacao_parametro
+
+    class Meta:
+        verbose_name = "Alerta"
+        verbose_name_plural = "Alertas"
+
+    def __str__(self):
+        return self.nome
+
+
+class HistoricoAlerta(Base):
+    timestamp = models.BigIntegerField(blank=False)
+    alerta = models.ForeignKey(Alerta, related_name="historico_alertas", on_delete=models.CASCADE)
+    timestamp_convertido = models.DateTimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.timestamp_convertido = datetime.fromtimestamp(self.timestamp)
+        super(HistoricoAlerta, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Histórico de Alerta"
+        verbose_name_plural = "Históricos de Alertas"
