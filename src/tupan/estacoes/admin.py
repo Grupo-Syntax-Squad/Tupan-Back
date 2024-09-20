2163c0f73261b415232424b626a4be7570814194
Iago Souza <iago.csouza15@gmail.com>
2024-09-20 10:54:42 -0300

    func: add endereco no admin

--- src/tupan/estacoes/admin.py
+++ src/tupan/estacoes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Estacao, Parametro
+from .models import Endereco, Estacao, Parametro
 
 @admin.register(Parametro)
 class ParametroAdmin(admin.ModelAdmin):
@@ -23,18 +23,59 @@ class ParametroAdmin(admin.ModelAdmin):
         "criado",
         "modificado"
     ]
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = [
+        "logradouro",
+        "numero",
+        "bairro",
+        "cep",
+        "cidade",
+        "estado",
+        "complemento",
+        "latitude",
+        "longitude",
+        "criado",
+        "modificado",
+    ]
+    list_filter = [
+        "cidade",
+    ]
+    readonly_fields = [
+        "criado",
+        "modificado"
+    ]
 
 @admin.register(Estacao)
 class EstacaoAdmin(admin.ModelAdmin):
     list_display = [
         "nome",
-        "endereco",
+        "getEndereco",
         "topico",
+        "getParametros",
         "ativo",
-        "parametros"
-        "criado",
-        "modificado",
     ]
+    list_filter = [
+        "modificado",
+        "nome",
+        "criado"
+    ]
+
+    readonly_fields = [
+        "criado",
+        "modificado"
+    ]
+
+    def getParametros(self, obj):
+        return ", ".join([parametro.nome for parametro in obj.parametros.all()])
+    
+    getParametros.short_description = 'Parâmetros'
+
+    def getEndereco(self, obj):
+        return obj.endereco.cep
+    
+    getEndereco.short_description = "Endereço"
+
     list_filter = [
         "modificado",
         "ativo",
