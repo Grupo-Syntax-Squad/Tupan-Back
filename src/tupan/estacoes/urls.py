7ac7307c4fb131cd8d6f39a771aa6d763e1c2186
Iago Souza <iago.csouza15@gmail.com>
2024-09-23 08:56:17 -0300

    func: add post, put e delete para estações, endereços e parametros

--- src/tupan/estacoes/urls.py
+++ src/tupan/estacoes/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import EstacoesView
+from .views import estacao_list, estacao_details, parametro_list, parametro_details, endereco_list, endereco_details
 
 urlpatterns = [
-    path("", EstacoesView.as_view(), name="Estações")
+    path("estacoes", estacao_list, name="Estações"),
+    path("estacoes/<int:pk>", estacao_details, name="Estação"),
+    path("enderecos", endereco_list, name="Endereços"),
+    path("enderecos/<int:pk>", endereco_details, name="Endereço"),
+    path("parametros", parametro_list, name="Parâmetros"),
+    path("parametros/<int:pk>", parametro_details, name="parâmetro")    
 ]
--- src/tupan/tupan/urls.py
+++ src/tupan/tupan/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estacoes', include('estacoes.urls'))
+    path('', include('estacoes.urls'))
 ]
