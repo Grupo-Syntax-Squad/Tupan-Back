2d498f8dd12d8154326ab8631ed2fe7709f0f5f0
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-09-23 08:29:48 -0300

    func: Adiciona rota de update de Usuario

--- src/tupan/usuarios/views.py
+++ src/tupan/usuarios/views.py
@@ -33,3 +33,13 @@ class UsuarioList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, format=None):
+        serializer = UsuarioSerializer(data=request.data)
+        if serializer.is_valid():
+            user = Usuario.get(id=serializer.data.id)
+            user.email = serializer.data.email
+            user.password = serializer.data.password
+            user.save()
+            return Response(user, status=status.HTTP_200_OK)
+        return Response("", status=status.HTTP_400_BAD_REQUEST)
