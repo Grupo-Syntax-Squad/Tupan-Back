64694fcc537a091dac2a007dade635700c186acc
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-09-23 08:29:48 -0300

    func: Adiciona rota de update de Usuario

--- src/tupan/usuarios/tests.py
+++ src/tupan/usuarios/tests.py
@@ -6,11 +6,11 @@ class TestUsuario:
     @pytest.mark.django_db
     def test_criacao_usuarios(self):
         user1 = Usuario.objects.create_user(email='user@gmail.com', password='senha1')
-        user2 = Usuario.objects.create_user(email='well@gmail.com', password='senha2')
+        user2 = Usuario.objects.create_user(email='user2@gmail.com', password='senha2')
         
         assert Usuario.objects.count() == 2
         assert user1.email == 'user@gmail.com'
-        assert user2.email == 'well@gmail.com'
+        assert user2.email == 'user2@gmail.com'
         
         assert user1.check_password("senha1")
         assert user2.check_password("senha2")
@@ -21,7 +21,7 @@ class TestUsuario:
     @pytest.mark.django_db
     def test_listagem_usuarios(self):
         user1 = Usuario.objects.create_user(email='user@gmail.com', password='senha1')
-        user2 = Usuario.objects.create_user(email='well@gmail.com', password='senha2')
+        user2 = Usuario.objects.create_user(email='user2@gmail.com', password='senha2')
         
         usuarios = Usuario.objects.all()
         assert len(usuarios) == 2
@@ -30,7 +30,17 @@ class TestUsuario:
     
     @pytest.mark.django_db
     def test_inativar_usuario(self):
-        user1 = Usuario.objects.create_user(email="wellingtonll.faria@gmail.com", password="123123")
-        user1.ativo = False
-        user1.save()
-        assert user1.ativo == False
+        user = Usuario.objects.create_user(email="user@gmail.com", password="123123")
+        user.ativo = False
+        user.save()
+        assert user.ativo == False
+    
+    @pytest.mark.django_db
+    def test_atualizar_usuario(self):
+        user = Usuario.objects.create_user(email="user@gmail.com", password="userpass")
+        user.email = "test@gmail.com"
+        user.password = "passwd"
+        user.save()
+
+        assert user.email == "test@gmail.com"
+        assert user.password == "passwd"        
