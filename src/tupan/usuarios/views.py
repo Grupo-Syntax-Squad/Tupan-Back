ce5cd5cd836c473e6b01dd20a4f7dca865de48e2
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-10-07 10:41:48 -0300

    mant: adicionando a documentacao dos endpoints de usuarios

--- src/tupan/usuarios/views.py
+++ src/tupan/usuarios/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiParameter, OpenApiTypes
 
 class UsuarioList(APIView):
     """
@@ -19,11 +20,24 @@ class UsuarioList(APIView):
             self.permission_classes = [IsAuthenticated]
         return super().get_permissions()
     
+    @extend_schema(
+        description="Lista todos os usuários ativos",
+        responses={200: UsuarioSerializer(many=True), 404: OpenApiResponse(description="Nenhum usuário cadastrado")}
+    )
     def get(self, request, format=None):
         usuarios = Usuario.objects.all().filter(ativo=True)
+        if not usuarios.exists():
+            return Response({"mensagem": "Nenhum usuário cadastrado"}, status=status.HTTP_404_NOT_FOUND)
         serializer = UsuarioSerializer(usuarios, many=True)
         return Response(serializer.data)
 
+    @extend_schema(
+            description="Deleta um usuário pelo seu id",
+            parameters=[
+                OpenApiParameter("id", OpenApiTypes.INT, description="Id do usuário", required=True)
+            ],
+            responses={200: UsuarioSerializer, 404: OpenApiResponse(description="Usuário não encontrado"), 400: OpenApiResponse(description="Erro na requisição")}
+    )
     def delete(self, request, format=None):
         try:
             id_usuario = request.data.get("id")
@@ -36,7 +50,12 @@ class UsuarioList(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-    
+        
+    @extend_schema(
+        description="Cria um novo usuário",
+        request=UsuarioSerializer,
+        responses={201: UsuarioSerializer, 400: OpenApiResponse(description="Erro na requisição")}
+    )
     def post(self, request, format=None):
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
@@ -44,10 +63,18 @@ class UsuarioList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, format=None):
+    @extend_schema(
+        description="Atualiza um usuário pelo seu id",
+        parameters=[
+            OpenApiParameter("id", OpenApiTypes.INT, description="Id do usuário", required=True)
+        ],
+        request=UsuarioSerializer,
+        responses={200: UsuarioSerializer, 404: OpenApiResponse(description="Usuário não encontrado"), 400: OpenApiResponse(description="Erro na requisição")}
+    )
+    def put(self, request, id, format=None):
         try:
             new_data = request.data
-            user = Usuario.objects.get(id=new_data["id"])
+            user = Usuario.objects.get(id)
             user.email = new_data["email"]
             user.password = new_data["password"]
             user.save()
