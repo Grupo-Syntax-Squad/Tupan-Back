a9a393ce7cd1817f46ca02671359229e5fb9fbad
WellingtonLFaria <wellingtonll.faria@gmail.com>
2024-10-09 10:44:03 -0300

    docs: Documentando views.py do app estacao

--- src/tupan/estacoes/views.py
+++ src/tupan/estacoes/views.py
@@ -3,8 +3,14 @@ from .serializers import EstacaoSerializer, EnderecoSerializer, ParametroSeriali
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse, OpenApiRequest, OpenApiTypes
 
 class EstacoesView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(EstacaoSerializer(many=True))
+        }
+    )
     def get(self, request, *args, **kwargs):
         ativo = request.query_params.get('ativo')
         estacoes = Estacao.objects.all()
@@ -12,6 +18,13 @@ class EstacoesView(APIView):
             estacoes = estacoes.filter(ativo=ativo)   
         serializer = EstacaoSerializer(estacoes, many=True)
         return Response(serializer.data)
+    @extend_schema(
+            request=OpenApiRequest(EstacaoSerializer),
+            responses={
+                201: OpenApiResponse(EstacaoSerializer()),
+                400: OpenApiResponse(description="Erro na requisição")
+            }
+    )
     def post(self, request, *args, **kwargs):
         serializer = EstacaoSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,6 +33,12 @@ class EstacoesView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class EstacoesDetalhesView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(EstacaoSerializer()),
+            404: OpenApiResponse(description="Estação não encontrada")
+        }
+    )
     def get(self, request, pk, *args, **kwargs):
         try:
             estacao = Estacao.objects.get(pk=pk)
@@ -27,6 +46,16 @@ class EstacoesDetalhesView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = EstacaoSerializer(estacao)
         return Response(serializer.data)
+    
+    @extend_schema(
+        request=OpenApiRequest(EstacaoSerializer()),
+        responses={
+            200: OpenApiResponse(EstacaoSerializer()),
+            400: OpenApiResponse(description="Erro na requisição"),
+            404: OpenApiResponse(description="Estação não encontrada")
+        }
+            
+    )
     def put(self, request, pk, *args, **kwargs):
         try:
             estacao = Estacao.objects.get(pk=pk)
@@ -37,6 +66,12 @@ class EstacoesDetalhesView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @extend_schema(
+        responses={
+            204: OpenApiResponse(description="Estação deletada com sucesso"),
+            404: OpenApiResponse(description="Estação não encontrada")
+        }
+    )
     def delete(self, request, pk, *args, **kwargs):
         try:
             estacao = Estacao.objects.get(pk=pk)
@@ -47,10 +82,22 @@ class EstacoesDetalhesView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class EnderecosView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(EnderecoSerializer(many=True))
+        }
+    )
     def get(self, request, *args, **kwargs):
         enderecos = Endereco.objects.all()
         serializer = EnderecoSerializer(enderecos, many=True)
         return Response(serializer.data)
+    @extend_schema(
+        request=OpenApiRequest(EnderecoSerializer),
+        responses={
+            201: OpenApiResponse(EnderecoSerializer()), 
+            400: OpenApiResponse(description="Erro na requisição")
+        }
+    )
     def post(self, request, *args, **kwargs):
         serializer = EnderecoSerializer(data=request.data)
         if serializer.is_valid():
@@ -59,6 +106,12 @@ class EnderecosView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class EnderecosDetalhesView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(EnderecoSerializer()), 
+            404: OpenApiResponse(description="Endereço não encontrado")
+        }
+    )
     def get(self, request, pk, *args, **kwargs):
         try:
             endereco = Endereco.objects.get(pk=pk)
@@ -66,6 +119,14 @@ class EnderecosDetalhesView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = EnderecoSerializer(endereco)
         return Response(serializer.data)
+    @extend_schema(
+        request=OpenApiRequest(EnderecoSerializer),
+        responses={
+            200: OpenApiResponse(EnderecoSerializer()), 
+            404: OpenApiResponse(description="Endereço não encontrado"), 
+            400: OpenApiResponse(description="Erro na requisição")
+        }
+    )
     def put(self, request, pk, *args, **kwargs):
         try:
             endereco = Endereco.objects.get(pk=pk)
@@ -76,6 +137,12 @@ class EnderecosDetalhesView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @extend_schema(
+        responses={
+            204: OpenApiResponse(description="Endereço deletado com sucesso"), 
+            404: OpenApiResponse(description="Endereço não encontrado")
+        }
+    )
     def delete(self, request, pk, *args, **kwargs):
         try:
             endereco = Endereco.objects.get(pk=pk)
@@ -85,10 +152,22 @@ class EnderecosDetalhesView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT) 
 
 class ParametrosView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(ParametroSerializer(many=True))
+        }
+    )
     def get(self, request, *args, **kwargs):
         parametros = Parametro.objects.all()
         serializer = ParametroSerializer(parametros, many=True)
         return Response(serializer.data)
+    @extend_schema(
+        request=OpenApiRequest(ParametroSerializer),
+        responses={
+            201: OpenApiResponse(ParametroSerializer()), 
+            400: OpenApiResponse(description="Erro na requisição")
+        }
+    )
     def post (self, request, *args, **kwargs):
         serializer = ParametroSerializer(data=request.data)
         if serializer.is_valid():
@@ -97,6 +176,12 @@ class ParametrosView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ParametrosDetalhesView(APIView):
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(ParametroSerializer()), 
+            404: OpenApiResponse(description="Parâmetro não encontrado")
+        }
+    )
     def get(self, request, pk, *args, **kwargs):
         try:
             parametro = Parametro.objects.get(pk=pk)
@@ -104,6 +189,14 @@ class ParametrosDetalhesView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = ParametroSerializer(parametro)
         return Response(serializer.data)
+    @extend_schema(
+        request=OpenApiRequest(ParametroSerializer),
+        responses={
+            200: OpenApiResponse(ParametroSerializer()), 
+            404: OpenApiResponse(description="Parâmetro não encontrado"), 
+            400: OpenApiResponse(description="Erro na requisição")
+        }
+    )
     def put(self, request, pk, *args, **kwargs):
         try:
             parametro = Parametro.objects.get(pk=pk)
@@ -114,6 +207,12 @@ class ParametrosDetalhesView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @extend_schema(
+        responses={
+            204: OpenApiResponse(description="Parâmetro deletado com sucesso"), 
+            404: OpenApiResponse(description="Parâmetro não encontrado")
+        }
+    )
     def delete(self, request, pk, *args, **kwargs):
         try:
             parametro = Parametro.objects.get(pk=pk)
--- src/tupan/usuarios/urls.py
+++ src/tupan/usuarios/urls.py
@@ -3,5 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from usuarios.views import UsuarioList
 
 urlpatterns = [
-    path('', UsuarioList.as_view())
+    path('', UsuarioList.as_view()),
+    path('/<int:pk>', UsuarioList.as_view())
 ]
