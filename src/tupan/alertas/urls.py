79168b5f8e701605150734aa82abb2da84d46dd8
Ryan <ryanarajo@yahoo.com.br>
2024-11-28 11:48:27 -0300

    func: criação das rodas para exportação CSV para medições e histórico de alertas

--- src/tupan/alertas/urls.py
+++ src/tupan/alertas/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import AlertasView, AlertasDetalhesView, HistoricoAlertaView, MedicaoView, MedicaoDetalhesView
+from .views import (
+    AlertasView,
+    AlertasDetalhesView,
+    HistoricoAlertaView,
+    MedicaoView,
+    MedicaoDetalhesView,
+    CSVHistoricoAlertasView,
+    CSVMedicaoView,
+    )
 
 urlpatterns = [
     path('alertas', AlertasView.as_view(), name='alertas'),
     path('alertas/<int:id>', AlertasDetalhesView.as_view(), name='alertas-detalhes'),
     path('historicos', HistoricoAlertaView.as_view(), name='historico-alertas'),
     path('medicoes', MedicaoView.as_view(), name='medicoes'),
-    path('medicoes/<int:id>', MedicaoDetalhesView.as_view(), name='medicoes-detalhes')
+    path('medicoes/<int:id>', MedicaoDetalhesView.as_view(), name='medicoes-detalhes'),
+    path('historicos/csv', CSVHistoricoAlertasView.as_view(), name='historico-csv'),
+    path('medicoes/csv', CSVMedicaoView.as_view(), name='medicoes-csv'),
 ]
